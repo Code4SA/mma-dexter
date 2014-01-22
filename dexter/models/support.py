e8d39e18554ba069f33d4d8fafc10496433b9aa0
Greg Kempe <greg@kempe.net>
2014-01-22 10:35:37 +0200

    Tweaks to basics, models.

--- dexter/models/support.py
+++ dexter/models/support.py
@@ -1,8 +1,7 @@
 from sqlalchemy import (
     Column,
-    Index,
     Integer,
-    Text,
+    UnicodeText,
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -16,12 +15,3 @@ from zope.sqlalchemy import ZopeTransactionExtension
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
-
-
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
-
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
--- dexter/scripts/initializedb.py
+++ dexter/scripts/initializedb.py
@@ -13,7 +13,6 @@ from pyramid.scripts.common import parse_vars
 
 from ..models import (
     DBSession,
-    MyModel,
     Base,
     )
 
@@ -28,13 +27,16 @@ def usage(argv):
 def main(argv=sys.argv):
     if len(argv) < 2:
         usage(argv)
+
     config_uri = argv[1]
     options = parse_vars(argv[2:])
     setup_logging(config_uri)
+
     settings = get_appsettings(config_uri, options=options)
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
+
     with transaction.manager:
-        model = MyModel(name='one', value=1)
-        DBSession.add(model)
+        # add initial model contents
+        pass
