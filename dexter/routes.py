e561385d3d2291cc729772c7aa04cd8dadbc2dde
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-02-12 11:47:20 +0200

    Add index page with a brief description and overview of the database.

--- dexter/routes.py
+++ dexter/routes.py
@@ -1,13 +1,37 @@
 from dexter.app import app
 from flask.ext.mako import render_template
+from sqlalchemy.sql import func
 
-from dexter.models import Document
+from dexter.models import db, Document, Entity, Medium
 
 import dexter.articles
 import dexter.entities
 
 @app.route('/')
 def home():
-    documents = Document.query.order_by(Document.published_at.desc()).limit(100)
+
+    document_count = Document.query.count()
+    if document_count is None:
+        document_count = 0
+
+    earliest = Document.query.order_by(Document.published_at).first()
+    latest = Document.query.order_by(Document.published_at.desc()).first()
+
+    group_counts = {}
+    for group_count, group_name in db.session.query(func.count(Entity.id), Entity.group).group_by(Entity.group).all():
+        group_counts[group_name] = int(group_count)
+
+    medium_counts = []
+    for medium_count, medium_name in db.session.query(func.count(Document.id), Medium.name) \
+            .join(Medium) \
+            .group_by(Document.medium_id) \
+            .order_by(func.count(Document.id)) \
+            .limit(5):
+        medium_counts.append([medium_name, int(medium_count)])
+
     return render_template('index.haml',
-            documents=documents)
+                           document_count=document_count,
+                           latest=latest.published_at.strftime('%e %B %Y'),
+                           earliest=earliest.published_at.strftime('%e %B %Y'),
+                           group_counts=group_counts,
+                           medium_counts=medium_counts)
