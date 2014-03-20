0cea4a83d8419cb67660c41232e142b38db14de1
Greg Kempe <greg@kempe.net>
2014-03-20 14:53:18 +0200

    Redirect non-admins to add page from landing page.

--- dexter/routes.py
+++ dexter/routes.py
@@ -1,6 +1,7 @@
 from dexter.app import app
+from flask import request, url_for, flash, redirect
 from flask.ext.mako import render_template
-from flask.ext.login import login_required
+from flask.ext.login import login_required, current_user
 from sqlalchemy.sql import func
 
 from dexter.models import db, Document, Entity, Medium
@@ -13,6 +14,14 @@ import dexter.users
 @app.route('/')
 @login_required
 def home():
+    if current_user.admin:
+        return redirect(url_for('dashboard'))
+    else:
+        return redirect(url_for('new_article'))
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
 
     latest_docs = Document.query.order_by(Document.created_at.desc()).limit(20)
 
@@ -35,11 +44,11 @@ def home():
     for medium_count, medium_name in db.session.query(func.count(Document.id), Medium.name) \
             .join(Medium) \
             .group_by(Document.medium_id) \
-            .order_by(func.count(Document.id)) \
+            .order_by(func.count(Document.id).desc()) \
             .limit(5):
         medium_counts.append([medium_name, int(medium_count)])
 
-    return render_template('index.haml',
+    return render_template('dashboard.haml',
                            latest_docs=latest_docs,
                            document_count=document_count,
                            latest=latest,
