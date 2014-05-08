63c9850ac380f057fcf30029be6e294e31fd1587
Greg Kempe <greg@kempe.net>
2014-05-08 10:22:12 +0200

    Adjust CSS and JS import and bundles.

--- assets/js/document.js
+++ assets/js/document.js
@@ -325,77 +325,9 @@
       $('input[name$="-deleted"]', $row).val('0');
     };
   };
-
-  Dexter.DocumentMaps = function() {
-    var self = this;
-
-    self.init = function() {
-      if ($('#slippy-map').length === 0) {
-        return;
-      }
-
-      self.map = L.map('slippy-map');
-      self.map.setView({lat: -28.4796, lng: 24.698445}, 5);
-
-      var osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        minZoom: 1,
-        maxZoom: 16,
-        attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'});	
-      self.map.addLayer(osm);
-
-      self.drawFeatures();
-
-      $('a[data-toggle="tab"][href="#places-tab"]').on('shown.bs.tab', function(e) {
-        self.map.invalidateSize(false);
-      });
-    };
-
-    self.drawFeatures = function() {
-      $('.geo-feature').each(function(i, feat) {
-        console.log(feat);
-
-        var $feat = $(feat);
-        var geo = $feat.data('geo-data');
-        var parts;
-
-        if ($feat.data('geo-type') == 'point') {
-          parts = geo.split(',');
-          var lat = parseFloat(parts[0]),
-              lng = parseFloat(parts[1]);
-
-          L.marker([lat, lng])
-            .addTo(self.map)
-            .bindPopup($('td:first-child', $feat).text());
-
-        } else {
-          parts = geo.split('-');
-          var level = parts[0],
-              code = parts[1];
-
-          d3.json("http://maps.code4sa.org/political/2011/" + level + '?filter[' + level + ']=' + code + '&quantization=5000', function(error, topo) {
-            if (!topo)
-              return;
-
-            var featureLayer = L.geoJson(topojson.feature(topo, topo.objects.demarcation), {
-              style: {
-                "clickable": false,
-                "color": "#00d",
-                "fillColor": "#ccc",
-                "weight": 1.0,
-                "opacity": 0.5,
-                "fillOpacity": 0.5,
-              },
-            });
-            self.map.addLayer(featureLayer);
-          });
-        }
-      });
-    };
-  };
 })(jQuery, window);
 
 $(function() {
   new Dexter.EditDocumentView().init();
   new Dexter.EditDocumentAnalysisView().init();
-  new Dexter.DocumentMaps().init();
 });
