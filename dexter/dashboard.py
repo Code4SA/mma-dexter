43ea988b4106c9855a64b8500a3d5750f043dfe4
Greg Kempe <greg@kempe.net>
2014-05-12 16:16:57 +0200

    Remove useless and slow details from landing page.

--- dexter/dashboard.py
+++ dexter/dashboard.py
@@ -27,36 +27,8 @@ def dashboard():
     for date, group in groupby(latest_docs, lambda d: d.created_at.date()):
         doc_groups.append([date, list(group)])
 
-    document_count = Document.query.count()
-    if document_count is None:
-        document_count = 0
-
-    earliest = Document.query.order_by(Document.published_at).first()
-    latest = Document.query.order_by(Document.published_at.desc()).first()
-    if earliest:
-        earliest = earliest.published_at.strftime('%e %B %Y')
-    if latest:
-        latest = latest.published_at.strftime('%e %B %Y')
-
-    group_counts = {}
-    for group_count, group_name in db.session.query(func.count(Entity.id), Entity.group).group_by(Entity.group).all():
-        group_counts[group_name] = int(group_count)
-
-    medium_counts = []
-    for medium_count, medium_name in db.session.query(func.count(Document.id), Medium.name) \
-            .join(Medium) \
-            .group_by(Document.medium_id) \
-            .order_by(func.count(Document.id).desc()) \
-            .limit(5):
-        medium_counts.append([medium_name, int(medium_count)])
-
     return render_template('dashboard/dashboard.haml',
-                           doc_groups=doc_groups,
-                           document_count=document_count,
-                           latest=latest,
-                           earliest=earliest,
-                           group_counts=group_counts,
-                           medium_counts=medium_counts)
+                           doc_groups=doc_groups)
 
 
 @app.route('/monitor-dashboard')
