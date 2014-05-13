f4c4cb1e1eaa4b7115a0eaff71feadfa9d113bbe
Greg Kempe <greg@kempe.net>
2014-05-13 14:18:47 +0200

    Partial work to use modals for person replacement

--- dexter/entities.py
+++ dexter/entities.py
@@ -56,15 +56,6 @@ def show_person(id):
             key=lambda t: t[1])
 
     if request.method == 'POST' and current_user.admin:
-        if 'mergein' in request.args:
-            # merge someone into this person
-            dup = Person.query.get(request.args['mergein'])
-            if dup:
-                dup.merge_into(person)
-                flash('Merged %s into %s.' % (dup.name, person.name))
-                db.session.commit()
-            return redirect(url_for('show_person', id=id))
-
         if form.validate():
             form.populate_obj(person)
 
@@ -76,10 +67,6 @@ def show_person(id):
             flash('Saved.')
             db.session.commit()
             return redirect(url_for('show_person', id=id))
-
-    # possible misspellings
-    dups = [p for p, _ in person.similarly_named_people(0.8)]
-    dups.sort(key=lambda p: p.name)
 
     # docs where this person is a source
     query = db.session.query(distinct(Document.id))\
@@ -99,8 +86,33 @@ def show_person(id):
         person=person,
         form=form,
         docs=docs,
-        dups=dups,
         pagination=pagination)
+
+@app.route('/people/<int:id>/merge', methods=['GET', 'POST'])
+@login_required
+def merge_person(id):
+    person = Person.query.get(id)
+    if not person:
+        return make_response("The specified entity could not be found.", 404)
+
+    if request.method == 'POST' and current_user.admin:
+        if 'mergein' in request.args:
+            # merge someone into this person
+            dup = Person.query.get(request.args['mergein'])
+            if dup:
+                dup.merge_into(person)
+                flash('Merged %s into %s.' % (dup.name, person.name))
+                db.session.commit()
+
+        return redirect(url_for('show_person', id=id))
+
+    # possible misspellings
+    dups = [p for p, _ in person.similarly_named_people(0.8)]
+    dups.sort(key=lambda p: p.name)
+
+    return render_template('person/merge.haml',
+        person=person,
+        dups=dups)
 
 @app.route('/people/new', methods=['POST'])
 @login_required
