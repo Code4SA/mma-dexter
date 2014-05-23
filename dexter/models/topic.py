9d43ea4bf64e617058275ee8202f2dfebf0b734d
Greg Kempe <greg@kempe.net>
2014-05-23 14:44:26 +0200

    Sort Topics by code

--- dexter/models/topic.py
+++ dexter/models/topic.py
@@ -23,10 +23,20 @@ class Topic(db.Model):
         return "<Topic name='%s'>" % (self.name.encode('utf-8'),)
 
 
+    def sort_key(self):
+        try:
+            # sort ("10. Foo", "10.2 Bar") by the code
+            parts = (self.group.split(' ', 1)[0], self.name.split(' ', 1)[0])
+
+            return [int(k.replace('.', '')) for k in parts]
+        except:
+            return (self.group, self.name)
+
+
     @classmethod
     def for_select_widget(cls, topics):
         choices = []
-        topics.sort(key=lambda t: [t.group, t.name])
+        topics.sort(key=cls.sort_key)
         for group, items in groupby(topics, lambda t: t.group):
             choices.append((group, [[str(t.id), t.name] for t in items]))
         return choices
@@ -79,57 +89,56 @@ class Topic(db.Model):
 1|Other (Last Resort)
 """
         text = """
-2|Economics - Includes business, corporate news, finance issues, trade agreements|Economics
-2|Politics (International) - Diplomacy efforts, political news from outside South Africa|Politics (International)
-2|Disaster/Accident - Earthquakes, famine, typhoons, accidents, tragedy|Disaster/Accident
-2|Conflict, Political Violence - Demonstration, Protests, War|Conflict, Political Violence
-2|Crime - Murder, robbery, hijacking, theft, Corruption-bribery, fraud at both government and corporate levels|Crime
-2|Justic system - Court rulings, constitutional issues, legislation, bills, amendments, judicial system|Justice System
-2|SA National Politics - Includes SA Gov & Parliament-national government, national issues, parliament, national politics|SA National Politics
-2|Provincial & Local Govt - Includes municipalities, policies affecting only certain provinces, local government finance|Provincial & Local Govt
-2|Housing - Includes policies, lack of housing, government initiatives|Housing
-2|Social Welfare - Policies on welfare grants, pension, child grants|Social Welfare
-2|Poverty Rate - Policies|Poverty rate
-2|Health - general health issues, diabetes, cancer, nutrition, excludes HIV/AIDS|Health
-2|Development - Policies, projects|Development
-2|Environment - Pollution, extinction of animal/plant species|Environment
-2|Media and arts - New media, freedom of expression, entertainment, culture-theatre, lifestyle issues, fashion, religion and tradition|Media and Arts
-2|Sport - news on sport events, reports, athletes, policies|Sport
-2|Personalities/Profiles - Features on prominent personalities or upcoming people|Personalities/Profiles
-2|Cultural practices and traditions|Cultural practicies and traditions
-2|Child Labour - exploitation of children for work as cheap labour|Child Abuse
-2|Child Pornography|Child Abuse
-2|General when codes below don't apply|Child Abuse
-2|Physical Abuse-beatings, burnings|Child Abuse
-2|Mental & Emotional Abuse-verbal and consistently making derogatory remarks|Child Abuse
-2|Child Prostitution-use of children for sex work|Child Abuse
-2|Child Abduction/ Trafficking/ Slavery - abducting a child for sexual purposes or slavery|Child Abuse
-2|Kidnapping- taking a child ifor ransom purposes|Child Abuse
-2|Child Rape-non-consensual includes penetrative and non-penetrative sex with a  minor includes statutory rape|Child Abuse
-2|Sexual Abuse- the abuse of boys and girls and included indecent assault and sodomy|Child Abuse
-2|Child Neglect-failure to adequately attend to a child's needs|Child Abuse
-2|Maintenance and child support-bills, divorce cases|Child Abuse
-2|Adoption - international local and other related issues|Adoption
-2|Education General - where the codes below do not apply.|Education
-2|Policy related: state of schools, education policies, etc.|Education
-2|Events and Achievements: school fun days, awards etc.|Education
-2|Violence: levels of violence among learners and school related disasters and tragedies.|Education
-2|Science-reports about new inventions, technology|Science
-2|Human Rights-includes a variety of rights|Human Rights
-2|Gender - where the central focus of the story is on a gender related element.|Gender
-2|Racism & Xenophobia- incidents of racism & discrimination based on a person's ethnicity or nationality|Racism and Xenophobia
-2|Disabilities-mental or physical|Disabilities
-2|Family-reports on values, the ideal family or focus on a specific family|Family
-2|HIV/AIDS - general when codes below don't apply|HIV/AIDS
-2|Aids Orphans/children affected by HIV, where children have parents/caregivers due to HIV/AIDS|Treatment of HIV/AIDS
-2|Sex Education-items on practicing safe sex in relation to HIV/AIDS and in STD's|Sex Education
-2|Treatment of HIV/AIDS-items relating to ARVs or nevirapine or treatment in general|Treatment of HIV/AIDS
-2|Funds-monies donated for the treatment of HIV|Funds
-2|Teenage pregnancy|Teenage Pregnancy
-2|Substance Abuse-drugs and alcohol|Substance Abuse
-2|Refugee children-asylum seekers, refugees rights|Refugee Children
-2|Other- to be used as a last resort|Z Other
-2|Not Relevant - for Television and Radio only|ZZ Not Relevant
+2|1.1. Adoption - international local and other related issues|1. Adoption
+2|2.1. Child Labour - exploitation of children for work as cheap labour|2. Child Abuse
+2|2.2. Child Pornography|2. Child Abuse
+2|2.3. General when codes below don't apply|2. Child Abuse
+2|2.4. Physical Abuse-beatings, burnings|2. Child Abuse
+2|2.5. Mental & Emotional Abuse-verbal and consistently making derogatory remarks|2. Child Abuse
+2|2.6. Child Prostitution-use of children for sex work|2. Child Abuse
+2|2.7. Child Abduction/ Trafficking/ Slavery - abducting a child for sexual purposes or slavery|2. Child Abuse
+2|2.8. Kidnapping- taking a child ifor ransom purposes|2. Child Abuse
+2|2.9. Child Rape-non-consensual includes penetrative and non-penetrative sex with a  minor includes statutory rape|2. Child Abuse
+2|2.10. Sexual Abuse- the abuse of boys and girls and included indecent assault and sodomy|2. Child Abuse
+2|2.11. Child Neglect-failure to adequately attend to a child's needs|2. Child Abuse
+2|2.12. Maintenance and child support-bills, divorce cases|2. Child Abuse
+2|3.1. Conflict, Political Violence - Demonstration, Protests, War|3. Conflict, Political Violence
+2|4.1. Crime - Murder, robbery, hijacking, theft, Corruption-bribery, fraud at both government and corporate levels|4. Crime
+2|5.1. Cultural practices and traditions|5. Cultural practicies and traditions
+2|6.1. Development - Policies, projects|6. Development
+2|7.1. Disabilities-mental or physical|7. Disabilities
+2|8.1. Disaster/Accident - Earthquakes, famine, typhoons, accidents, tragedy|8. Disaster/Accident
+2|9.1. Economics - Includes business, corporate news, finance issues, trade agreements|9. Economics
+2|10.1.  Education General - where the codes below do not apply.|10. Education
+2|10.2. Policy related: state of schools, education policies, etc.|10. Education
+2|10.3. Events and Achievements: school fun days, awards etc.|10. Education
+2|10.4. Violence: levels of violence among learners and school related disasters and tragedies.|10. Education
+2|11.1. Environment - Pollution, extinction of animal/plant species|11. Environment
+2|12.1. Family-reports on values, the ideal family or focus on a specific family|12. Family
+2|13.1. Funds-monies donated for the treatment of HIV|13. Funds
+2|14.1. Gender - where the central focus of the story is on a gender related element.|14. Gender
+2|15.1. Health - general health issues, diabetes, cancer, nutrition, excludes HIV/AIDS|15. Health
+2|16.1. Aids Orphans/children affected by HIV, where children have parents/caregivers due to HIV/AIDS|16. HIV/AIDS
+2|16.2. Treatment of HIV/AIDS-items relating to ARVs or nevirapine or treatment in general|16. HIV/AIDS
+2|16.3. Sex Education-items on practicing safe sex in relation to HIV/AIDS and in STD's|16. HIV/AIDS
+2|16.4. HIV/AIDS - general when other codes  don't apply|16. HIV/AIDS
+2|17.1. Housing - Includes policies, lack of housing, government initiatives|17. Housing
+2|18.1. Human Rights-includes a variety of rights|18. Human Rights
+2|19.1. Justice system - Court rulings, constitutional issues, legislation, bills, amendments, judicial system|19. Justice System
+2|20.1. Media and arts - New media, freedom of expression, entertainment, culture-theatre, lifestyle issues, fashion, religion and tradition|20. Media and Arts
+2|21.1. Personalities/Profiles - Features on prominent personalities or upcoming people|21. Personalities/Profiles
+2|22.1. Politics (International) - Diplomacy efforts, political news from outside South Africa|22. Politics
+2|22.2. SA National Politics - Includes SA Gov & Parliament-national government, national issues, parliament, national politics|22. Politics
+2|22.3. Provincial & Local Govt - Includes municipalities, policies affecting only certain provinces, local government finance|22. Politics
+2|23.1. Poverty Rate - Policies|23. Poverty rate
+2|24.1. Racism & Xenophobia- incidents of racism & discrimination based on a person's ethnicity or nationality|24. Racism and Xenophobia
+2|25.1. Refugee children-asylum seekers, refugees rights|25. Refugee Children
+2|26.1. Science-reports about new inventions, technology|26. Science
+2|27.1. Social Welfare - Policies on welfare grants, pension, child grants|27. Social Welfare
+2|28.1. Sport - news on sport events, reports, athletes, policies|28. Sports
+2|29.1. Substance Abuse-drugs and alcohol|29. Substance Abuse
+2|30.1. Teenage pregnancy|30. Teenage Pregnancy
+2|31.1. Other- to be used as a last resort|31. Other
         """
 
         topics = []
