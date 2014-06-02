9c20b745829be1718b9178984a5f8c9ae3c91c58
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-06-02 21:19:25 +0200

    Clickable provinces & municipalities.

--- assets/js/coveragemap.js
+++ assets/js/coveragemap.js
@@ -25,12 +25,15 @@
       console.log("no municipality selected")
 
     //Width and height
-    var w = 800;
-    var h = 600;
+    var width = 750;
+    var height = 600;
+    var active = d3.select(null);
+
+    var transition_duration = 300;
 
     //Define map projection
     var projection = d3.geo.mercator()
-      .translate([w/2, h/2])
+      .translate([width/2, height/2])
       .center([25.48, -28.76])
       .scale([2000]);
 
@@ -41,27 +44,112 @@
     //Create SVG element
     var svg = d3.select("#coverage-map")
       .append("svg")
-      .attr("width", w)
-      .attr("height", h);
+      .attr("width", width)
+      .attr("height", height);
 
-    self.init = function() {
-      d3.json("https://maps.code4sa.org/political/2011/province?filter&quantization=5000", function(error, topo) {
+    svg.append("rect")
+      .attr("class", "background")
+      .attr("width", width)
+      .attr("height", height)
+      .on("click", reset);
+
+    var g = svg.append("g")
+      .style("stroke-width", "1.5px");
+
+    function clicked(d) {
+      if (active.node() === this) return reset();
+      var feature_id = d.id;
+      var feature_name = null
+      if(d.properties.hasOwnProperty('province_name'))
+      {
+        feature_name = d.properties.province_name;
+        load_municipalities(feature_id)
+      }
+      else if(d.properties.hasOwnProperty('municipality_name'))
+        feature_name = d.properties.municipality_name;
+
+      var centroid = projection(d3.geo.centroid(d));
+      console.log(feature_id)
+
+      active.classed("active", false);
+      active = d3.select(this).classed("active", true);
+
+      var bounds = path.bounds(d),
+        dx = bounds[1][0] - bounds[0][0],
+        dy = bounds[1][1] - bounds[0][1],
+        x = (bounds[0][0] + bounds[1][0]) / 2,
+        y = (bounds[0][1] + bounds[1][1]) / 2,
+        scale = .9 / Math.max(dx / width, dy / height),
+        translate = [width / 2 - scale * x, height / 2 - scale * y];
+
+      g.transition()
+        .duration(transition_duration)
+        .style("stroke-width", 1.5 / scale + "px")
+        .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
+    }
+
+    function reset() {
+      active.classed("active", false);
+      active = d3.select(null);
+
+      g.transition()
+        .duration(transition_duration)
+        .style("stroke-width", "1.5px")
+        .attr("transform", "");
+    }
+
+    function load_provinces(){
+      d3.json("https://maps.code4sa.org/political/2011/province?filter&quantization=2000", function(error, topo) {
         if (error){
-          $("#slippy-map").text("No data available.")
+          $("#coverage-map").text("No data available.")
           return
         }
 
         //Bind data and create one path per GeoJSON feature
-        svg.selectAll("path")
+        g.selectAll("path.province")
           .data(topojson.feature(topo, topo.objects.demarcation).features)
           .enter()
           .append("path")
-          .attr("class", function(d) { return "province " + d.id; })
+          .attr("class", function(d) { return "feature province " + d.id; })
+          .attr("d", path)
+          .on("click", clicked);
+
+        g.append("path")
+          .datum(topojson.mesh(topo, topo.objects.demarcation, function(a, b) { return a !== b; }))
+          .attr("class", "mesh province")
           .attr("d", path);
       });
+    }
 
+    function load_municipalities(province_id){
+      d3.json("https://maps.code4sa.org/political/2011/municipality?filter&quantization=1000&filter[province]=" + province_id, function(error, topo) {
+        if (error){
+          $("#coverage-map").text("No data available.")
+          return
+        }
+        
+        g.selectAll("path.municipality").remove()
+
+        //Bind data and create one path per GeoJSON feature
+        g.selectAll("path.municipality")
+          .data(topojson.feature(topo, topo.objects.demarcation).features)
+          .enter()
+          .append("path")
+          .attr("class", function(d) { return "feature municipality " + d.id; })
+          .attr("d", path)
+          .on("click", clicked);
+
+        g.append("path")
+          .datum(topojson.mesh(topo, topo.objects.demarcation, function(a, b) { return a !== b; }))
+          .attr("class", "mesh municipality")
+          .attr("d", path);
+      });
+    }
+
+    self.init = function() {
+      load_provinces()
       self.load_and_draw_chart()
-    };
+    }
 
     self.load_and_draw_chart = function(){
       // load chart data
