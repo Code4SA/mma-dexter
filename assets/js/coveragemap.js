774a1f2e37ade315e078f7c1ea17b53ef95ab3aa
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-06-03 08:15:39 +0200

    Update chart after clicking area.

--- assets/js/coveragemap.js
+++ assets/js/coveragemap.js
@@ -10,7 +10,12 @@
     var form = $(".coverage-refine")
     var input_selected_province = form.find("input#selected_province")
     var input_selected_municipality = form.find("input#selected_municipality")
-    var span_title = $("#map-area-title")
+    var input_published_at = form.find("input#published_at")
+
+    var map_title = $("#coverage-map-title")
+    var map_subtitle = $("#coverage-map-subtitle")
+    var chart_title = $("#medium-breakdown-chart-title")
+    var chart_subtitle = $("#medium-breakdown-chart-subtitle")
 
     var selected_province = input_selected_province.val()
     var selected_municipality = input_selected_municipality.val()
@@ -56,24 +61,7 @@
     var g = svg.append("g")
       .style("stroke-width", "1.5px");
 
-    function clicked(d) {
-      if (active.node() === this) return reset();
-      var feature_id = d.id;
-      var feature_name = null
-      if(d.properties.hasOwnProperty('province_name'))
-      {
-        feature_name = d.properties.province_name;
-        load_municipalities(feature_id)
-      }
-      else if(d.properties.hasOwnProperty('municipality_name'))
-        feature_name = d.properties.municipality_name;
-
-      var centroid = projection(d3.geo.centroid(d));
-      console.log(feature_id)
-
-      active.classed("active", false);
-      active = d3.select(this).classed("active", true);
-
+    function pan_and_zoom(d){
       var bounds = path.bounds(d),
         dx = bounds[1][0] - bounds[0][0],
         dy = bounds[1][1] - bounds[0][1],
@@ -88,10 +76,58 @@
         .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
     }
 
+    function update_selection(province_id, municipality_id){
+      input_selected_province.val(province_id);
+      input_selected_municipality.val(municipality_id);
+      selected_province = input_selected_province.val()
+      selected_municipality = input_selected_municipality.val()
+    }
+
+    function click_province(d) {
+      if (active.node() === this) return reset();
+      var feature_id = d.id;
+      var feature_name = d.properties.province_name;
+
+      load_municipalities(feature_id);
+
+      active.classed("active", false);
+      active = d3.select(this).classed("active", true);
+
+      map_title.text(feature_name)
+      update_selection(feature_id, null)
+      self.load_and_draw_chart()
+
+      pan_and_zoom(d);
+    }
+
+    function click_municipality(d) {
+      if (active.node() === this) return reset();
+      var feature_id = d.id;
+      var feature_name = d.properties.municipality_name;
+
+      active.classed("active", false);
+      active = d3.select(this).classed("active", true);
+
+      map_title.text(feature_name)
+      update_selection(selected_province, feature_id)
+      self.load_and_draw_chart()
+
+      pan_and_zoom(d);
+    }
+
     function reset() {
       active.classed("active", false);
       active = d3.select(null);
 
+      // reset map title and chart
+      map_title.text("South Africa")
+      update_selection(null, null)
+      self.load_and_draw_chart()
+
+      // remove municipality shapes
+      g.selectAll("path.municipality").remove()
+
+      // zoom out
       g.transition()
         .duration(transition_duration)
         .style("stroke-width", "1.5px")
@@ -112,8 +148,9 @@
           .append("path")
           .attr("class", function(d) { return "feature province " + d.id; })
           .attr("d", path)
-          .on("click", clicked);
+          .on("click", click_province);
 
+        // draw lines around shapes
         g.append("path")
           .datum(topojson.mesh(topo, topo.objects.demarcation, function(a, b) { return a !== b; }))
           .attr("class", "mesh province")
@@ -127,7 +164,7 @@
           $("#coverage-map").text("No data available.")
           return
         }
-        
+
         g.selectAll("path.municipality").remove()
 
         //Bind data and create one path per GeoJSON feature
@@ -137,8 +174,9 @@
           .append("path")
           .attr("class", function(d) { return "feature municipality " + d.id; })
           .attr("d", path)
-          .on("click", clicked);
+          .on("click", click_municipality);
 
+        // draw lines around shapes
         g.append("path")
           .datum(topojson.mesh(topo, topo.objects.demarcation, function(a, b) { return a !== b; }))
           .attr("class", "mesh municipality")
@@ -152,6 +190,10 @@
     }
 
     self.load_and_draw_chart = function(){
+      // update map subtitle
+      var published_at = input_published_at.val()
+      if(published_at)
+        map_subtitle.text(published_at)
       // load chart data
       $.getJSON(self.placesUrl(), function(data){
         console.log(data)
@@ -160,9 +202,15 @@
         if(selected_municipality)
           data = data['municipalities'][selected_municipality]
         if(data)
+        {
+          chart_subtitle.text("(" + data.total + " articles)");
           self.drawChart(data);
+        }
         else
+        {
           $(".chart.chart-media-coverage").text("No data available.")
+          chart_subtitle.text("");
+        }
       });
     }
 
@@ -223,7 +271,7 @@
         },
         yAxis: {
           title: {
-            text: 'number of stories'
+            text: '# Articles'
           }
         },
         series: [{
