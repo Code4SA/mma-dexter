e4c7b30ab1b089689c4841cf817712b6ef58f1b0
Greg Kempe <greg@kempe.net>
2014-06-12 10:11:10 +0200

    Use S3 as a backend store.

--- dexter/attachments.py
+++ dexter/attachments.py
@@ -5,6 +5,7 @@ from flask.ext.uploads import patch_request_class
 
 from sqlalchemy_imageattach.context import (pop_store_context, push_store_context)
 from sqlalchemy_imageattach.stores.fs import HttpExposedFileSystemStore
+from sqlalchemy_imageattach.stores.s3 import S3Store
 
 log = logging.getLogger(__name__)
 
@@ -14,19 +15,10 @@ def setup_attachments(app):
     # max upload size
     patch_request_class(app, 6 * 1024 * 1024)
 
-    # setup the file storage for image attachments
-    # TODO: handle testing
-    path = '/tmp/dexter-attachments'
-    try:
-        os.makedirs(path)
-    except:
-        pass
-    log.info("Storing attachments in %s" % path)
-    # TODO: production
-    store = HttpExposedFileSystemStore(path, '/static-attachments/')
-    app.wsgi_app = store.wsgi_middleware(app.wsgi_app)
+    # attachment store
+    store = setup_store(app)
 
-
+    # link attachment store implicitly to the request chain
     @app.before_request
     def start_implicit_store_context():
         global pushed
@@ -39,3 +31,35 @@ def setup_attachments(app):
         if pushed:
             pop_store_context()
             pushed = False
+
+def setup_store(app):
+    # setup the file storage for image attachments
+    store_type = app.config.get('ATTACHMENT_STORE', 'disk')
+
+    if store_type == "s3":
+        # use S3
+        bucket = app.config['ATTACHMENT_S3_BUCKET']
+        prefix = app.config.get('ATTACHMENT_S3_PREFIX', '')
+
+        log.info("Storing attachments in S3 at %s/%s" % (bucket, prefix))
+
+        store = S3Store(bucket,
+                app.config['AWS_S3_ACCESS_KEY'],
+                app.config['AWS_S3_SECRET_KEY'],
+                prefix=prefix)
+
+    elif store_type == "disk":
+        # TODO: handle testing
+        path = '/tmp/dexter-attachments'
+        try:
+            os.makedirs(path)
+        except:
+            pass
+        log.info("Storing attachments in %s" % path)
+        store = HttpExposedFileSystemStore(path, '/static-attachments/')
+        app.wsgi_app = store.wsgi_middleware(app.wsgi_app)
+
+    else:
+      raise ValueError("Attachment store type should be one of 'disk' or 's3', not '%s'" % store_type)
+
+    return store
