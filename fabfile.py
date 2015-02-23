649f43f804d5c4815f6c081d7fea0ac76633aa8b
Greg Kempe <greg@kempe.net>
2015-02-23 11:59:11 +0200

    Deploy to /home/ubuntu

--- fabfile.py
+++ fabfile.py
@@ -6,7 +6,7 @@ from fabric.contrib.files import exists, upload_template
 
 VIRTUALENV_DIR = 'env'
 CODE_DIR = 'mma-dexter'
-PROD_HOSTS = ['mma-dexter.code4sa.org']
+PROD_HOSTS = ['ec2-52-16-43-97.eu-west-1.compute.amazonaws.com']
 
 PACKAGES = [
     'build-essential',
@@ -19,18 +19,19 @@ PACKAGES = [
     'libmagickwand-dev',
     'nginx',
     'rabbitmq-server',
-    'python-numpy',
-    'python-scipy',
     ]
 
 @task
 def prod():
     env.deploy_type = 'prod'
     # this must be an absolute directory
-    env.deploy_dir = '/home/mma/'
+    env.deploy_dir = '/home/ubuntu/'
     env.branch = 'master'
     env.hosts = PROD_HOSTS
-    env.user = 'mma'
+    env.user = 'ubuntu'
+
+    env.repo_dir = os.path.join(env.deploy_dir, CODE_DIR)
+    env.ve_dir = os.path.join(env.deploy_dir, CODE_DIR, VIRTUALENV_DIR)
 
 
 @task
@@ -39,42 +40,45 @@ def provision():
     sudo('apt-get install --yes --no-upgrade %s' % ' '.join(PACKAGES))
     sudo('pip install virtualenv')
 
+    with prefix('. %s/bin/activate' % env.ve_dir):
+        # numpy must be installed before requirements.txt, otherwise
+        # scikit-learn doesn't process its dependencies correctly
+        # and complains that numpy isn't there
+        run('pip install numpy==1.9.1')
+
 
 @task
 def deploy():
     require('deploy_type', 'deploy_dir', 'branch', provided_by=[prod])
 
-    repo_dir = os.path.join(env.deploy_dir, CODE_DIR)
-    ve_dir = os.path.join(env.deploy_dir, CODE_DIR, VIRTUALENV_DIR)
-
-    if not exists(repo_dir):
+    if not exists(env.repo_dir):
         with cd(env.deploy_dir):
             run('git clone https://github.com/Code4SA/mma-dexter.git')
 
-    if not exists(ve_dir):
-        run('virtualenv -p python2.7 --no-site-packages %s' % ve_dir)
+    if not exists(env.ve_dir):
+        run('virtualenv -p python2.7 --no-site-packages %s' % env.ve_dir)
 
-    with cd(repo_dir):
+    with cd(env.repo_dir):
         run('git checkout -B %s' % env.branch)
         run('git pull origin %s' % env.branch)
 
-    with cd(repo_dir), prefix('. %s/bin/activate' % ve_dir):
+    with cd(env.repo_dir), prefix('. %s/bin/activate' % env.ve_dir):
         run('pip install -r requirements.txt')
 
     # make sure logging dir exists and update processes
-    log_dir = os.path.join(repo_dir, 'log')
+    log_dir = os.path.join(env.repo_dir, 'log')
     run('mkdir -p %s' % log_dir)
 
     # log rotation
-    sudo('ln -fs %s/resources/logrotate/dexter /etc/logrotate.d/' % repo_dir)
+    sudo('ln -fs %s/resources/logrotate/dexter /etc/logrotate.d/' % env.repo_dir)
 
     # link in nginx config
-    sudo('ln -fs %s/resources/nginx/dexter.conf /etc/nginx/sites-enabled/' % repo_dir)
+    sudo('ln -fs %s/resources/nginx/dexter.conf /etc/nginx/sites-enabled/' % env.repo_dir)
     sudo('service nginx reload')
 
     # link in upstart config
-    sudo('ln -fs %s/resources/upstart/dexter.conf /etc/init/' % repo_dir)
-    sudo('ln -fs %s/resources/upstart/dexter-celery.conf /etc/init/' % repo_dir)
+    sudo('ln -fs %s/resources/upstart/dexter.conf /etc/init/' % env.repo_dir)
+    sudo('ln -fs %s/resources/upstart/dexter-celery.conf /etc/init/' % env.repo_dir)
     sudo('initctl reload-configuration')
 
     # restart dexter
