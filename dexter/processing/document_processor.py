d14ae580992224cdbecc198960617a91529a53de
Alex Blake <alex@assemble.co.za>
2017-03-27 10:57:27 +0200

    uncommented alchemy - couldnt test this extractor at the moment due to it being depricated, will need confimation from ODD if it all working fine

--- dexter/processing/document_processor.py
+++ dexter/processing/document_processor.py
@@ -44,7 +44,7 @@ class DocumentProcessor:
             # must come last
             GenericCrawler()]
         self.extractors = [
-            # AlchemyExtractor(),
+            AlchemyExtractor(),
             CalaisExtractor(),
             SourcesExtractor(),
             PlacesExtractor()]
