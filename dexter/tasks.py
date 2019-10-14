92049ea3a9e3e9f4b3aa1bbc7407d4181e56e5c3
Matt Adendorff <matthew@tenji.co>
2019-10-14 12:32:26 +0200

    Storage re-run

--- dexter/tasks.py
+++ dexter/tasks.py
@@ -20,8 +20,8 @@ log = logging.getLogger(__name__)
 def back_process_feeds():
     """ Enqueue a task to fetch yesterday's feeds. """
 
-    if date.today() == date(2019, 6, 6):
-        d1 = date(2019, 6, 3)
+    if date.today() == date(2019, 10, 15):
+        d1 = date(2019, 10, 13)
         d2 = date(2019, 4, 21)
         # days = [d1 + timedelta(days=x) for x in range((d2 - d1).days + 1)]
         days = [d1]
