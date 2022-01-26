71554067936e2355658e6e566e8fcb4a66f24ee7
Matt Adendorff <matthew@opendata.durban>
2022-01-26 11:31:14 +0200

    Add new keyfile

--- dexter/config/celeryconfig.py
+++ dexter/config/celeryconfig.py
@@ -21,14 +21,14 @@ CELERY_ENABLE_UTC = True
 
 CELERYBEAT_SCHEDULE = {
     'fetch-yesterdays-feeds': {
-        'schedule': crontab(hour=3, minute=0),
+        'schedule': crontab(hour=1, minute=0),
         'task': 'dexter.tasks.fetch_yesterdays_feeds',
     },
     'back-process-feeds': {
         'schedule': crontab(hour=11, minute=0),
         'task': 'dexter.tasks.back_process_feeds',
     },
-    fetch_yesterdays_feeds_rerun: {
+    'fetch_yesterdays_feeds_rerun': {
         'schedule': crontab(hour=12, minute=0),
         'task': 'dexter.tasks.back_process_feeds',
     },
