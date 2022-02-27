b9e1138410cd216dd8537431e23045ad6dfd9b3f
Matt Adendorff <matthew@tenji.co>
2022-02-27 10:02:30 +0200

    Add remove tasks

--- dexter/config/celeryconfig.py
+++ dexter/config/celeryconfig.py
@@ -21,7 +21,7 @@ CELERY_ENABLE_UTC = True
 
 CELERYBEAT_SCHEDULE = {
     'fetch-yesterdays-feeds': {
-        'schedule': crontab(hour=3, minute=0),
+        'schedule': crontab(hour=11, minute=0),
         'task': 'dexter.tasks.fetch_yesterdays_feeds',
     },
     # 'back-process-feeds': {
