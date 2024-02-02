d32e83d042ba88a30b8b0e06b5e7e922b1a40c59
Matt Adendorff <matthew@tenji.co>
2024-02-02 11:26:18 +0200

    Update NT

--- dexter/tasks.py
+++ dexter/tasks.py
@@ -138,17 +138,17 @@ def fetch_daily_feeds(self, day):
             if count <= 499:
                 get_feed_item.delay(item, 0)
             elif count <= 999:
-                get_feed_item.delay(item, 1)
-            elif count <= 1499:
                 get_feed_item.delay(item, 2)
-            elif count <= 1999:
+            elif count <= 1499:
                 get_feed_item.delay(item, 3)
-            elif count <= 2499:
+            elif count <= 1999:
                 get_feed_item.delay(item, 4)
-            elif count <= 2999:
+            elif count <= 2499:
                 get_feed_item.delay(item, 5)
-            elif count <= 3499:
+            elif count <= 2999:
                 get_feed_item.delay(item, 6)
+            # elif count <= 3499:
+            #     get_feed_item.delay(item, 6)
             else:
                 get_feed_item.delay(item, 0)
 
